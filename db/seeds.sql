95cdb9845173e6410981f8b726670aff829c477d
David Sotomayor <david.elijah.sotomayor@gmail.com>
2021-02-15 23:30:01 -0600

    modified schema and seeds file; preparing to create database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ( "Cheeseburger" , false);
+INSERT INTO burgers (burger_name, devoured) VALUES ( "Double Butterburger" , false);
+INSERT INTO burgers (burger_name, devoured) VALUES ( "BLT Burger" , false);
