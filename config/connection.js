1106b132e62d69250e294c15599d7717f6d6318b
David Sotomayor <david.elijah.sotomayor@gmail.com>
2021-02-16 23:49:04 -0600

    heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require('mysql');
+let connection;
+if (process.env.JAWSDB_URL) {
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Luna",
-  database : 'burgers_db',
-  port: 3306
-});
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Luna",
+    database : 'burgers_db',
+    port: 3306
+  });
+}
 
 connection.connect( function(err) {
   if (err) throw err;
